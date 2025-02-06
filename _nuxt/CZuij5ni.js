756e5ffeac2fba3465fce0dd75e352f9b2c81349
ics-konno <52394590+ics-konno@users.noreply.github.com>
2025-02-06 02:16:25 +0000

    Deploying to gh-pages from @ ics-creative/250204_nuxt_studio@4bf084276d445bcfefc1dbb1998a9db590ca5700 🚀

--- _nuxt/CZuij5ni.js
+++
@@ -1,1 +0,0 @@
-import{a as r,f as a,k as o,Z as s,ag as l,l as n}from"./D84lR6wj.js";const c=r({__name:"Alert",props:{color:{type:String,default:"orange"}},setup(e){return(t,d)=>(o(),a("div",{class:"alert",style:l({"border-color":e.color})},[s(t.$slots,"default",{},void 0,!0)],4))}}),p=Object.assign(n(c,[["__scopeId","data-v-46dbbe98"]]),{__name:"Alert"});export{p as default};
